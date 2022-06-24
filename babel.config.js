4d960eec1e65e5719b63de64a706bce135d60704
João Andrade <joaovictorvieira.23@hotmail.com>
2022-06-24 16:00:29 -0300

    feat: create input component

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,22 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      // 'react-native-reanimated/plugin',
       [
-        'module-resolver',
+        "module-resolver",
         {
-          root: ['./src'],
+          root: ["./src"],
           extensions: [
-            '.ts',
-            '.tsx',
-            '.js',
-            '.json'
+            ".ts",
+            ".tsx",
+            ".js",
+            ".json"
           ],
           alias: {
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@assets': './src/assets',
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@assets": "./src/assets",
           }
         }
       ]
