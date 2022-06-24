ef46dbf4429ccae337f2d8239946073374b0db43
João Andrade <joaovictorvieira.23@hotmail.com>
2022-06-24 15:02:10 -0300

    feat: creation sign in

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from 'styled-components/native';
 
 import theme from './src/theme';
 
+import { SignIn } from '@screens/SignIn';
+
 export default function App() {
 
   const [fontsLoaded] = useFonts({
@@ -19,7 +21,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme} >
-
+      <SignIn />
     </ThemeProvider>
   );
 };
