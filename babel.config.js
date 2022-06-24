48d06990c069b8a95a12d5b2e130f9a5280b9d7c
João Andrade <joaovictorvieira.23@hotmail.com>
2022-06-24 16:08:39 -0300

    chore: tweak in babel file

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
+      'react-native-reanimated/plugin',
       [
-        "module-resolver",
+        'module-resolver',
         {
-          root: ["./src"],
+          root: ['./src'],
           extensions: [
-            ".ts",
-            ".tsx",
-            ".js",
-            ".json"
+            '.ts',
+            '.tsx',
+            '.js',
+            '.json'
           ],
           alias: {
-            "@components": "./src/components",
-            "@screens": "./src/screens",
-            "@assets": "./src/assets",
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@assets': './src/assets',
           }
         }
       ]
