662c266c9859dfd550097cd0df1ae8e252e80293
João Andrade <joaovictorvieira.23@hotmail.com>
2022-06-28 12:45:01 -0300

    feat: install image picker

--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-import { Button } from '@src/components/Button';
+import { Button } from '@components/Button';
 
 export const Container = styled.KeyboardAvoidingView`
     flex: 1;
