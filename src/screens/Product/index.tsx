9e7ed0ba76daf1cc6b2470c798bd9b54d2d7a7d5
João Andrade <joaovictorvieira.23@hotmail.com>
2022-06-28 12:36:19 -0300

    feat: upload section

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
 
 import { ButtonBack } from '@components/ButtonBack';
+import { Photo } from '@components/Photo';
 
-import { Container, Header, Title, DeleteLabel } from './styles';
+import { 
+    Container, 
+    Header, 
+    Title, 
+    DeleteLabel,
+    Upload,
+    PickImageButton
+} from './styles';
 
 export function Product() {
     return (
@@ -15,6 +23,11 @@ export function Product() {
                     <DeleteLabel>Deletar</DeleteLabel>
                 </TouchableOpacity>
             </Header>
+            <Upload>
+                <Photo uri="" />
+
+                <PickImageButton title="Carregar" type="secondary" />
+            </Upload>
         </Container>
     );
 }
--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -2,6 +2,8 @@ import styled, { css } from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
+import { Button } from '@src/components/Button';
+
 export const Container = styled.KeyboardAvoidingView`
     flex: 1;
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
@@ -35,3 +37,16 @@ export const DeleteLabel = styled.Text`
         color: ${theme.COLORS.TITLE};
     `}
 `
+
+export const Upload = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: 32px 0;
+`;
+
+export const PickImageButton = styled(Button)`
+    max-width: 90px;
+    margin-left: 32px;
+`;
