c5c91de62b988e0aa89ccedd9bdccef8ca432176
João Andrade <joaovictorvieira.23@hotmail.com>
2022-06-30 11:36:46 -0300

    feat: list orders

--- src/components/OrderCard/index.tsx
+++ src/components/OrderCard/index.tsx
@@ -11,22 +11,32 @@ import {
     StatusTypesProps
 } from './styles';
 
+export type OrderProps = {
+    id: string;
+    pizza: string;
+    image: string;
+    status: StatusTypesProps;
+    table_number: string;
+    quantity: string;
+}
+
 type Props = TouchableOpacityProps & {
     index: number;
+    data: OrderProps;
 };
 
-export function OrderCard({ index, ...rest }: Props) {
+export function OrderCard({ index, data, ...rest }: Props) {
     return (
         <Container index={index} {...rest}>
-            <Image source={{ uri: 'https://github.com/joaovieira23.png' }} />
-            <Name>4 Queijos</Name>
+            <Image source={{ uri: data.image }} />
+            <Name>{data.pizza}</Name>
 
             <Description>
-                Mesa 5 ⚬ Qnt: 1
+                {`Mesa ${data.table_number} - Qnt: ${data.quantity}`}
             </Description>
 
-            <StatusContainer status='Preparando'>
-                <StatusLabel status='Preparando'>Preparando</StatusLabel>
+            <StatusContainer status={data.status}>
+                <StatusLabel status={data.status}>{data.status}</StatusLabel>
             </StatusContainer>
         </Container>
     )
